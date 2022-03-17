b69cbe382605f68bae2f75d62f9b110d0287626f
aidahyseni <48920906+aidahyseni@users.noreply.github.com>
2022-03-17 20:03:25 +0100

    Update OpenWeatherMapAPISteps.java

--- src/test/java/stepDefinition/OpenWeatherMapAPISteps.java
+++ src/test/java/stepDefinition/OpenWeatherMapAPISteps.java
@@ -2,7 +2,6 @@ package stepDefinition;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.Assert;
@@ -13,49 +12,44 @@ public class OpenWeatherMapAPISteps {
 
     @Given("I set GET weather service api for London")
     public void i_set_get_weather_service_api_for_london() {
-        Response resp=RestAssured.get("https://api.openweathermap.org/data/2.5/forecast?q=london&appid=6096ffa7b82c16c8eb721154561cff3f");
+        Response resp=RestAssured.get("https://api.openweathermap.org/data/2.5/onecall?lat=51.5085&lon=-0.1257&exclude=minutely,hourly&appid=22d22ac5b842b1d39ee15e571564a988");
        String responsebo = resp.getBody().asString();
-       //String[] ss= responsebo.split(",");
-       //JsonArray body=new JsonArray(responsebo);
-       //for (int i=0; i<body.size(); i++){
-           //JsonObject bb = body.getAsJsonObject(i);
-       boolean temp_min = responsebo.startsWith("temp_min");
-        System.out.println("Response Body for London is:"+temp_min);
-//String ss= responsebo.startsWith("temp_min");
+
+        System.out.println("Response Body for London is:"+responsebo);
         int code=resp.getStatusCode();
         System.out.println("Status code is :"+code);
         Assert.assertEquals(code,200);
 
     }
-    @When("I set request HEADER")
-    public void i_set_request_header() {
 
-
-
-    }
-    @Then("I recieve valid HTTP response")
-    public void i_recieve_valid_http_response() {
-
-    }
     @Then("I set Get weather api for Pristina")
     public void i_set_get_weather_api_for_pristina() {
-        Response resp=RestAssured.get("https://api.openweathermap.org/data/2.5/forecast?q=pristina&appid=6096ffa7b82c16c8eb721154561cff3f");
+        Response resp=RestAssured.get("https://api.openweathermap.org/data/2.5/onecall?lat=42.667542&lon=21.166191&exclude=minutely,hourly&appid=22d22ac5b842b1d39ee15e571564a988");
         String responsebo = resp.getBody().asString();
         System.out.println("Response Body For Pristina is:"+responsebo);
+        int code=resp.getStatusCode();
+        System.out.println("Status code is :"+code);
+        Assert.assertEquals(code,200);
 
     }
     @Then("I set Get weather api for Oslo")
     public void i_set_get_weather_api_for_oslo() {
-        Response resp=RestAssured.get("https://api.openweathermap.org/data/2.5/forecast?q=Oslo&appid=6096ffa7b82c16c8eb721154561cff3f");
+        Response resp=RestAssured.get("https://api.openweathermap.org/data/2.5/onecall?lat=59.911491&lon=10.757933&exclude=minutely,hourly&appid=22d22ac5b842b1d39ee15e571564a988");
         String responsebo = resp.getBody().asString();
         System.out.println("Response Body For Oslo is:"+responsebo);
+        int code=resp.getStatusCode();
+        System.out.println("Status code is :"+code);
+        Assert.assertEquals(code,200);
     }
 
     @Then("I set Get weather api for Sydney")
     public void i_set_get_weather_api_for_sydney() {
-        Response resp=RestAssured.get("https://api.openweathermap.org/data/2.5/forecast?q=Sydney&appid=6096ffa7b82c16c8eb721154561cff3f");
+        Response resp=RestAssured.get("https://api.openweathermap.org/data/2.5/onecall?lat=-33.865143&lon=151.209900&exclude=minutely,hourly&appid=22d22ac5b842b1d39ee15e571564a988");
         String responsebo = resp.getBody().asString();
         System.out.println("Response Body For Sydney is:"+responsebo);
+        int code=resp.getStatusCode();
+        System.out.println("Status code is :"+code);
+        Assert.assertEquals(code,200);
 
     }
 }
